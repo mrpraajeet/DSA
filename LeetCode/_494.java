c1030800deb61b5e34632d9895a623d0c9f3ff16
Praajeet <mrpraajeet777@gmail.com>
2024-12-26 21:50:39 +0530

    Did LeetCode 494

---
+++ LeetCode/_494.java
@@ -0,0 +1,10 @@
+public class _494 {
+    public int findTargetSumWays(int[] nums, int k) {
+        return exp(nums, k, 0, 0);
+    }
+
+    private int exp(int[] nums, int k, int i, int sum) {
+        if(i == nums.length) return sum == k ? 1 : 0;
+        return exp(nums, k, i+1, sum+nums[i]) + exp(nums, k, i+1, sum-nums[i]);
+    }
+}
